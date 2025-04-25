1e279e947625ff7040fab1d62411248992412550
NemuiMaou <djromar96@gmail.com>
2025-04-24 20:15:40 -0700

    Working/jared shop (#24)
    * Idea for Shop Logic implemented
    
    Can't seem to figure out how to fully implement yet. Having lots of trouble transferring this idea into the actual UI.
    
    * Script Cleanup

---
+++ Assets/Scripts/Shop/ItemPack.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class ItemPack : MonoBehaviour
+{
+    public string packName;     
+    public string description;  
+    public int price;           
+    public Sprite icon;         
+    public bool isBought;       
+}
