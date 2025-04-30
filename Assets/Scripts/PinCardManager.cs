230c139e72a6a38158fa7657713b1adb9aa1d7c4
Jackie Luc <165213092+ImagineWagons8487@users.noreply.github.com>
2025-04-29 17:22:16 -0700

    Results screen, randomized layout hand, score goals, cash implemented, boss stage functionality, need access to UI score goal element (#31)
    * Added results screen that goes to the bowling score screen then goes to shop, added bare bones Ante system that just increments a number when needed, created randomized hand of pin layouts every round
    
    * implemented score goal, throw type displaying in ui, results knowing about cash needed to add to balance, need access to goal score in UI to porperly display
    
    * added point goals, will need access to UI element to properly display, added boss stage functionality, every 3rd blind will have increased point goal, cash given and UI will say 'Boss Stage', added lose detection

---
+++ Assets/Scripts/PinCardManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PinCardManager : MonoBehaviour
+{
+    public static PinCardManager Instance { get; private set; }
+    
+    [SerializeField] private List<PinLayoutCardSO> pinLayoutCardSOs;
+
+    [SerializeField] private List<GameObject> pinCards;
+
+    [SerializeField] int cardGenerationLimit = 3;
+
+    private void Start()
+    {
+        Instance = this;
+        
+        //empty the list before starting
+        EndSelection();
+        //create selection before the first throw
+        StartSelection();
+    }
+
+    /// <summary>
+    /// Will generate cards up to cardGenerationLimit for the player to select for the next round
+    /// </summary>
+    public void StartSelection()
+    {
+        //randomly generates 3 card to choose from
+        
+        for (int i = 0; i < cardGenerationLimit; i++)
+        {
+            //creates a random int first in valid range
+            int curr = Random.Range(0, pinLayoutCardSOs.Count);
+            //instantiate card as a child of current object transform
+            GameObject currentCard = Instantiate(pinLayoutCardSOs[curr].prefab, transform);
+            //add SO to list of current layout of pin cards
+            pinCards.Add(currentCard);
+        }
+    }
+
+    public void EndSelection()
+    {
+        foreach (var pinCard in pinCards)
+        {
+            Destroy(pinCard);
+        }
+        pinCards.Clear();
+    }
+    
+}
