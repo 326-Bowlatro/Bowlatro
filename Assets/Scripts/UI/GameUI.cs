e6e1aff23e507f7be1d929033202a773aa44858a
Eli <elitkramer@gmail.com>
2025-05-01 21:10:46 -0700

    Replace Options->Exit (#43)

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static GameManager;
@@ -20,7 +19,19 @@ public class GameUI : MonoBehaviour
         var uiDocument = GetComponent<UIDocument>();
         rootElement = uiDocument.rootVisualElement;
 
-        // Setup button handlers
+        // Setup button handlers (left panel)
+        rootElement.Q<Button>("_LeftPanel_Exit").clicked += () =>
+        {
+#if UNITY_EDITOR
+            // Exit play mode in the editor
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // Exit application in the final build
+            Application.Quit();
+#endif
+        };
+
+        // Setup button handlers (shop)
         rootElement.Q<Button>("_Shop_Continue").clicked += () =>
         {
             // Go to playing state
@@ -28,6 +39,7 @@ public class GameUI : MonoBehaviour
         };
         rootElement.Q<Button>("_Shop_Reroll").clicked += () =>
         {
+            // Spend cash, reset shop. Ignore if not enough cash.
             if (GameManager.Instance.Cash >= ShopManager.RerollCost)
             {
                 GameManager.Instance.DeductCash(ShopManager.RerollCost);
