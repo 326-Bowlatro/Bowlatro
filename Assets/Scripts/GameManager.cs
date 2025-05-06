5d2cb05536887560f5f46ac4c093035944a6c1ea
BitCrusader <elikramergm@gmail.com>
2025-05-05 22:00:49 -0700

    Fix layout packs not spawning new cards

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -253,6 +253,7 @@ public class GameManager : StateMachine<GameManager, GameManager.PreRoundState>
         if (
             (CurrentScore >= CurrentScoreToBeat && !IsBossStage)
             || (IsBossStage && CurrentScore >= CurrentBossScoreToBeat)
+            || true /* TEMP DO NOT COMMIT ME! */
         )
         {
             EndBlind();
