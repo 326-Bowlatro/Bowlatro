7d336ac603a04e33c9ee3cd0f60f82fbb89f416e
Eli <elitkramer@gmail.com>
2025-05-06 18:44:05 -0700

    Rip out most of our dead code (#57)
    * Rip out most of our dead code
    
    * More dead code

--- Assets/Scripts/JokerEffect.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class JokerEffect : ScriptableObject
-{
-    public string jokerName;
-    public Sprite icon;
-
-    public abstract void ApplyEffect(GameObject context);
-}
